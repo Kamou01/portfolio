69feaca84fcb5f60c7531e522491754824627be7
Kamogelo Mkonto <82159075+Kamou01@users.noreply.github.com>
2021-05-14 16:27:43 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const navToggle = document.querySelector("nav-toggle");
+const navlinks = document.querySelectorAll(".nav__links");
+
+navToggle.addEventListener("click", () => {
+  document.body.classList.toggle("nav-open");
+});
+
+navlinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.body.classList.remove("nav-open");
+  });
+});
